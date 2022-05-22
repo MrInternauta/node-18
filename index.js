57168b19d3e99146c27ffcdb9caba46abf2fdc9c
Felipe Ramirez <feldjesus@gmail.com>
2022-05-22 09:27:00 -0500

    :sparkles: Testing node 18

---
+++ index.js
@@ -0,0 +1,33 @@
+import test from 'node:test';
+import assert from 'node:assert';
+
+test('Example', () => {
+  assert.equal(1, 1)
+})
+
+test('Example', () => {
+  assert.equal(1, 0, 'No match')
+})
+
+
+test('Example object', () => {
+  assert.deepEqual({ a: 1 }, { a: 1 }, 'No match obj')
+})
+
+
+test('async Example object', async () => {
+  const num = await Promise.resolve(1)
+  assert.equal(num, 1, 'No match obj')
+})
+
+test('Try test with findLast', async () => {
+  const nums = [1, 3, 4, 5, 8]
+  const lastEven = nums.findLast(n => n % 2 === 0);
+  assert.equal(lastEven, 8, 'Fails')
+})
+
+test('Try test with findLastIndex', async () => {
+  const nums = [1, 3, 4, 5, 8]
+  const lastEvenIndex = nums.findLastIndex(n => n % 2 === 0);
+  assert.equal(lastEvenIndex, 4, 'Fails')
+})
