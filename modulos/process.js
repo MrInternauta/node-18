d29caf85a810694498299843ab36337d98dbdd52
Felipe Ramirez <feldjesus@gmail.com>
2022-05-25 23:42:10 -0500

    :art: Add process file

--- modulos/process.js
+++ modulos/process.js
@@ -1,11 +1,41 @@
-const { exec } = require('node:child_process');
+import { exec, spawn } from 'node:child_process';
 
-exec('ls -la', (err, stdout, sterr) => {
-  if (err) {
-    console.error(err);
-    return false;
-  }
+// exec('ls -la', (err, stdout, sterr) => {
+//   if (err) {
+//     console.error(err);
+//     return false;
+//   }
 
-  console.log(stdout);
+//   console.log(stdout);
 
+// })
+
+//execute another node file
+// exec('node modulos/console.js', (err, stdout, sterr) => {
+//   if (err) {
+//     console.error(err);
+//     return false;
+//   }
+
+//   console.log(stdout);
+
+// })
+
+//strong
+let proceso = spawn('ls', ['-la'])
+console.log(proceso.pid);
+// console.log(proceso.connected);
+//listeting changes
+proceso.stdout.on('data', function (data) {
+  console.log(proceso.killed);
+  console.log(data.toString());
+  proceso.kill() //kill process
+  console.log(proceso.killed);
+
+
+})
+
+//OnExit
+proceso.on('exit', function () {
+  console.log('Finished');
 })
